81d53474f2d03126c1988d3cb1d24bf5a04ec2b0
gabri94 <gabrihacker@gmail.com>
2016-05-15 15:45:22 +0200

    Moved the C interface inside graph_parser. Updated makefile

--- graph-parser/src/graph_parser.h
+++ graph-parser/src/graph_parser.h
@@ -9,7 +9,6 @@
 #include "betweenness_centrality_heuristic.h"
 #include "parser.h"
 
-
 class graph_parser
 {
 public:
@@ -26,3 +25,30 @@ private:
   BetweennessCentralityHeuristic bcc;
   bool heuristic;
 };
+
+
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+typedef struct _id_bc_pair{
+	char * id;
+	double bc;
+
+}id_bc_pair;
+
+typedef void c_graph_parser;
+
+c_graph_parser* new_graph_parser(int weight, int heuristic);
+
+void graph_parser_parse_netjson(c_graph_parser* v, char *json);
+void graph_parser_calculate_bc(c_graph_parser* v);
+void graph_parser_compose_bc_map(c_graph_parser* v, id_bc_pair *map);
+void graph_parser_parse_jsoninfo(c_graph_parser* v, char *json);
+void delete_my_class(c_graph_parser* v);
+
+
+#ifdef __cplusplus
+}
+#endif
