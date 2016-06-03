ae615d58edc20f6ab8f50fb7201105b615a2d03f
Gabriele Gemmi <gabriel@autistici.org>
2016-06-03 18:18:41 +0200

    Implementation of dynamic loading of the plugins
    A common interface for all the Routing Protocol has been defined.
    It contains the methods: get_topology and push_timers

--- prince/src/olsr.c
+++ prince/src/olsr.c
@@ -1,4 +1,6 @@
-#include "olsr.h"
+#include "plugin_interface.h"
+#include "common.h"
+#include "socket.h"
 
 //PUBLIC FUNCTIONS
 /**
@@ -7,22 +9,14 @@
  * @param proto type of the remote plugin (0->netjson 1->jsoninfo)
  * @return pointer to olsr plugin handler
  */
-olsr_routing_plugin*
-new_olsr_plugin(char* host, c_graph_parser *gp, int proto){
-	olsr_routing_plugin *o = (olsr_routing_plugin *) malloc(sizeof(olsr_routing_plugin));
-	switch(proto){
-		case 0:
-			o->port=2005;
-		break;
-
-		case 1:
-			o->port=9090;
-		break;
-	}
-	o->proto=proto;
+routing_plugin*
+new_plugin(char* host, c_graph_parser *gp, int json_type){
+	routing_plugin *o = (routing_plugin *) malloc(sizeof(routing_plugin));
+	o->port=9090;
 	o->host=(char*)malloc(strlen(host)*sizeof(char));
 	strcpy(o->host, host);
 	o->gp = gp;
+	o->json_type=json_type;
 	return o;
 }
 
@@ -33,29 +27,31 @@ new_olsr_plugin(char* host, c_graph_parser *gp, int proto){
  * @return 1 if success, 0 otherwise
  */
 int
-get_olsr_topology(olsr_routing_plugin *o){
+get_topology(routing_plugin *o){ //netjson & jsoninfo
 	int sd = _create_socket(o->host, o->port);
 	char *req;
 	int sent;
-	switch(o->proto){
-	case 0:
+	switch(o->json_type){
+	case 1:
 		//netjson
 		req = "/NetworkGraph";
 		sent = write(sd,req,strlen(req));
-		if(!_receive_data(sd, &(o->recv_buffer))){
+		if(!_receive_data(sd, &(o->recv_buffer)))
 			return 0;
-		}
 		graph_parser_parse_netjson(o->gp, o->recv_buffer);
 		break;
 
-	case 1:
+	case 0:
+		//jsoninfo
 		req = "/topology";
 		sent = write(sd,req,strlen(req));
-		if(!_receive_data(sd, &(o->recv_buffer))){
+		if(!_receive_data(sd, &(o->recv_buffer)))
 			return 0;
-		}
 		graph_parser_parse_jsoninfo(o->gp, o->recv_buffer);
 		break;
+
+	default:
+		return 0;
 	}
 	return 1;
 }
@@ -69,7 +65,7 @@ get_olsr_topology(olsr_routing_plugin *o){
  * @return 1 if success, 0 otherwise
  */
 int
-olsr_push_timers(olsr_routing_plugin *o, struct timers t){
+push_timers(routing_plugin *o, struct timers t){
 	//TODO: push h and tc value to the daemon
 	return 0;
 }
@@ -79,7 +75,7 @@ olsr_push_timers(olsr_routing_plugin *o, struct timers t){
  * @param olsr plugin handler object
  */
 void
-delete_olsr_plugin(olsr_routing_plugin* o){
+delete_plugin(routing_plugin* o){
 	delete_graph_parser(o->gp);
 	free(o->host);
 	free(o->recv_buffer);
---
+++ prince/src/plugin_interface.h
@@ -0,0 +1,19 @@
+/*
+ * plugin_interface.h
+ *
+ *  Created on: 02 giu 2016
+ *      Author: gabriel
+ */
+#ifndef SRC_PLUGIN_INTERFACE_H_
+#define SRC_PLUGIN_INTERFACE_H_
+
+#include "common.h"
+
+
+routing_plugin* new_plugin(char* host, c_graph_parser *gp, int json_type);
+int get_topology(routing_plugin *o);
+int push_timers(routing_plugin *o, struct timers t);
+void delete_plugin(routing_plugin* o);
+
+
+#endif
