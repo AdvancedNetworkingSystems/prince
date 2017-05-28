478be9346c3e84759394dfe3b96beee736b45850
mb03 <mb0392@gmail.com>
2017-05-28 22:05:42 +0200

    fixed test file

--- graph-parser_c/src/test.c
+++ graph-parser_c/src/test.c
@@ -13,11 +13,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "../../prince/src/parser.h"
+#include "graph_parser.h"
 
-// #include "../../prince/src/parser.h"
-// #include "graph_parser.h"
-
-#include "biconnected.h"
 
 char * read_file_content(char * filename)
 {
@@ -66,7 +64,7 @@ char * read_file_content(char * filename)
 
     return buffer;
 }
-
+/*
 int main()
 {
     struct graph g;
@@ -177,44 +175,39 @@ int main()
 
     return 0;
 }
-
-/*
- * int main(int argc, char** argv) {
- * if(argc==1)
- * return 0;
- * int heuristic=atoi(argv[1]);
- * //to remove in case of different test
- * multithread=false;
- * if(argc==3){
- * stop_computing_if_unchanged=atoi(argv[2])==1;
- * }
- * c_graph_parser* cgp=new_graph_parser(1, heuristic);
- * char * file_content=read_file_content("input.json");
- * struct topology *topo=parse_netjson(file_content);
- * free(file_content);
- * graph_parser_parse_simplegraph(cgp,topo);
- * graph_parser_calculate_bc(cgp);
- * map_id_degree_bc * bc_degree_map = (map_id_degree_bc *)
- * malloc(sizeof(map_id_degree_bc));
- * graph_parser_compose_degree_bc_map(cgp,bc_degree_map);
- *
- * int i;
- * // printf("[\n");
- * printf("{\n");
- * for(i=0; i<bc_degree_map->size; i++){
- * //if(strcmp(bc_degree_map->map[i].id, node_name)==0){
- * printf("%s:%1.50f",bc_degree_map->map[i].id,bc_degree_map->map[i].bc);
- * //   break;
- * //  }
- * if(i<bc_degree_map->size-1)
- * printf(",");
- * printf("\n");
- * }
- * printf("}");
- * delete_graph_parser(cgp);
- * return (EXIT_SUCCESS);
- * }
- *
- */
-
+*/
+int main(int argc, char** argv) {
+if(argc==1)
+return 0;
+int heuristic=atoi(argv[1]);
+//to remove in case of different test
+multithread=false;
+if(argc==3){
+stop_computing_if_unchanged=atoi(argv[2])==1;
+}
+c_graph_parser* cgp=new_graph_parser(1, heuristic);
+char * file_content=read_file_content("input.json");
+struct topology *topo=parse_netjson(file_content);
+free(file_content);
+graph_parser_parse_simplegraph(cgp,topo);
+graph_parser_calculate_bc(cgp);
+map_id_degree_bc * bc_degree_map = (map_id_degree_bc *)
+malloc(sizeof(map_id_degree_bc));
+graph_parser_compose_degree_bc_map(cgp,bc_degree_map);
+int i;
+// printf("[\n");
+printf("{\n");
+for(i=0; i<bc_degree_map->size; i++){
+//if(strcmp(bc_degree_map->map[i].id, node_name)==0){
+printf("%s:%1.50f",bc_degree_map->map[i].id,bc_degree_map->map[i].bc);
+//   break;
+//  }
+if(i<bc_degree_map->size-1)
+printf(",");
+printf("\n");
+}
+printf("}");
+delete_graph_parser(cgp);
+return (EXIT_SUCCESS);
+}
 
