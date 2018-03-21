6ec73404f4b10a4217469c680817be51abeae0fa
Edoardo Putti <edoardo.putti@gmail.com>
2018-03-21 20:07:01 +0100

    format nicely control flow, add nicer errors and log messages

--- prince/src/prince.c
+++ prince/src/prince.c
@@ -18,89 +18,98 @@ void (*delete_plugin_p)(routing_plugin* o);
 int main(int argc, char* argv[])
 {
 	FILE *log=0;
-	if(argc<2){
-		printf("No conf file specified. Exiting.\n");
+	if (argc < 2) {
+		fprintf(stderr, "No conf file specified. Exiting.\n");
 		return -1;
 	}
-	printf("Prince Started\n");
-	prince_handler_t ph= new_prince_handler(argv[1]);
-	if(ph==0)
+	fprintf(stdout, "Prince Started\n");
+	prince_handler_t ph = new_prince_handler(argv[1]);
+	if (ph == INVALID_PRINCE_HANDLER) {
+                fprintf(stderr, "Could not start prince, got a bad handler\n");
 		return -1;
-	if(ph->log_file){
+        }
+
+	if (ph->log_file) {
 		log = fopen(ph->log_file, "a+");
-		if(log == NULL){
-			printf("Can't open log file\n");
+		if (log == NULL) {
+			printf("Could not open log file <%s>; Continuing without\n", ph->log_file);
 			free(ph->log_file);
 			ph->log_file = 0;
-		}else
-			printf("Logging in %s\n", ph->log_file);
+		} else {
+			printf("Logging to <%s>\n", ph->log_file);
+                }
 	}
 
-	recursive=ph->recursive;
-	multithread=ph->multithreaded;
-	stop_computing_if_unchanged=ph->stop_unchanged;
+	recursive = ph->recursive;
+	multithread = ph->multithreaded;
+
+	stop_computing_if_unchanged = ph->stop_unchanged;
+
 	signal(SIGPIPE, signal_callback_handler);
+
 	ph->gp = new_graph_parser(ph->weights, ph->heuristic);
 	int go = 1;
-	struct graph_parser * gp_p=(struct graph_parser *)ph->gp ;
+	struct graph_parser * gp_p = (struct graph_parser *) ph->gp;
 	ph->rp = new_plugin_p(ph->host, ph->port, ph->gp, ph->json_type, ph->timer_port);
-	do{
+	do {
 		sleep(ph->refresh);
-	}while(!get_initial_timers_p(ph->rp, &ph->def_t));
+	} while(!get_initial_timers_p(ph->rp, &ph->def_t));
 
-	do{
+	do {
 		sleep(ph->refresh);
-		if(!get_topology_p(ph->rp)){
-			printf("Error getting topology\n");
+		if (!get_topology_p(ph->rp)) {
+			fprintf(stderr, "Error getting topology\n");
 			sleep(ph->sleep_onfail);
 			continue;
 		}
 		graph_parser_parse_simplegraph(ph->rp->gp, ph->rp->t);
 		free_topo(ph->rp->t);
-		if(ph->rp->self_id!=0)
+		if (ph->rp->self_id != 0) {
 			free(ph->rp->self_id);
-		ph->rp->self_id=strdup(ph->rp->t->self_id);
+                }
+		ph->rp->self_id = strdup(ph->rp->t->self_id);
 
-		if(ph->rp->self_id){
-			if(ph->self_id!=0)
-			free(ph->self_id);
+		if (ph->rp->self_id) {
+			if (ph->self_id != 0) {
+                                free(ph->self_id);
+                        }
 			ph->self_id = strdup(ph->rp->self_id);
 		}
 		clock_t start = clock();
 		graph_parser_calculate_bc(ph->gp);
 		clock_t end = clock();
 		ph->bc_degree_map = (map_id_degree_bc *) malloc(sizeof(map_id_degree_bc));
-		ph->bc_degree_map->size=gp_p->g.nodes.size;
-		ph->bc_degree_map->map=0;
+		ph->bc_degree_map->size = gp_p->g.nodes.size;
+		ph->bc_degree_map->map = 0;
 		graph_parser_compose_degree_bc_map(ph->gp, ph->bc_degree_map);
-		ph->opt_t.exec_time = (double)(end - start) / CLOCKS_PER_SEC;
+		ph->opt_t.exec_time = (double) (end - start) / CLOCKS_PER_SEC;
 		ph->opt_t.centrality = get_self_bc(ph);
-		if(log){
+		if (log) {
 			log = fopen(ph->log_file, "a+");
 			struct timeval tv;
-			gettimeofday(&tv,NULL);
+			gettimeofday(&tv, NULL);
 			fprintf(log, "%i\t%4.4f\t%4.4f\t%4.4f\t%4.4f\n", tv.tv_sec, ph->opt_t.tc_timer, ph->opt_t.h_timer, ph->opt_t.exec_time, ph->opt_t.centrality);
 			fclose(log);
 		}
-		if (!compute_timers(ph)){
+		if (!compute_timers(ph)) {
 			printf("Can't find myself in topology! Help i'm lost\n");
 			sleep(ph->sleep_onfail);
 			continue;
 		}
-		if (!push_timers_p(ph->rp, ph->opt_t)){
+		if (!push_timers_p(ph->rp, ph->opt_t)) {
 			sleep(ph->sleep_onfail);
 			continue;
 		}
 		free(gp_p->bc);
-		gp_p->bc=0;
+		gp_p->bc = 0;
 		free_bc_degree_map(ph->bc_degree_map);
 		free_graph(&(gp_p->g));
 		init_graph(&(gp_p->g));
 		fflush(stdout);
-	}while(go);
+	} while (go);
 	free_prince_handler(ph);
-	printf("Prince Exited\n");
-	if(log){
+	fprintf(stdout, "Prince Exited\n");
+	if (log) {
 		log = fopen(ph->log_file, "a+");
 		fprintf(log, "Prince Exited\n");
 		fclose(log);
@@ -124,13 +133,16 @@ prince_handler_t new_prince_handler(char * conf_file)
 	ph->refresh=-1;
 	ph->self_id=0;
 	ph->sleep_onfail = 1;
-	if(read_config_file(ph, conf_file)==0)
-		return 0;
+
+	if (read_config_file(result, conf_file) == 0) {
+		return INVALID_PRINCE_HANDLER;
+        }
+
 	char libname[20] = "libprince_";
 	strcat(libname, ph->proto);
 	strcat(libname, ".so");
-	ph->plugin_handle = dlopen (libname, RTLD_LAZY);
-	if(!ph->plugin_handle)
+	result->plugin_handle = dlopen(libname, RTLD_LAZY);
+	if (result->plugin_handle == NULL) {
 		return 0;
 	new_plugin_p = (routing_plugin* (*)(char* host, int port, c_graph_parser *gp, int json_type, int timer_port)) dlsym(ph->plugin_handle, "new_plugin");
 	get_initial_timers_p =(int (*)(routing_plugin *o, struct timers *t)) dlsym(ph->plugin_handle, "get_initial_timers");
@@ -168,17 +180,17 @@ int compute_constants(prince_handler_t ph)
 	map_id_degree_bc *m_degree_bc = ph->bc_degree_map;
 	struct timers t = ph->def_t;
 	int degrees=0, i;
-	for(i=0; i<m_degree_bc->size;i++){
+	for(i = 0; i < m_degree_bc->size; i++){
 		degrees+=m_degree_bc->map[i].degree;
 		/*printf("%s %f\n", m_degree_bc->map[i].id, m_degree_bc->map[i].bc);*/
 	}
-	ph->c.R = m_degree_bc->n_edges;
-	ph->c.O_H = degrees/t.h_timer;
-	ph->c.O_TC = m_degree_bc->size*ph->c.R/t.tc_timer;
+	ph->c.R    = m_degree_bc->n_edges;
+	ph->c.O_H  = degrees / t.h_timer;
+	ph->c.O_TC = m_degree_bc->size*ph->c.R / t.tc_timer;
 	double sqrt_sum1=0, sqrt_sum2=0;
-	for(i=0; i<m_degree_bc->size; i++){
-		sqrt_sum1+=sqrt(m_degree_bc->map[i].degree * m_degree_bc->map[i].bc);
-		sqrt_sum2+=sqrt(ph->c.R*m_degree_bc->map[i].bc);
+	for (i = 0; i < m_degree_bc->size; i++) {
+		sqrt_sum1 += sqrt(m_degree_bc->map[i].degree * m_degree_bc->map[i].bc);
+		sqrt_sum2 += sqrt(ph->c.R*m_degree_bc->map[i].bc);
 	}
 	ph->c.sq_lambda_H = sqrt_sum1/ph->c.O_H;
 	ph->c.sq_lambda_TC = sqrt_sum2/ph->c.O_TC;
