c8f48468a6fdb4af3d364f392cc419328dc0f255
Edoardo Putti <edoardo.putti@gmail.com>
2018-03-23 11:23:55 +0100

    move plugin type declaration to common header

--- prince/include/common.h
+++ prince/include/common.h
@@ -36,5 +36,11 @@ routing_plugin_{
 
 } routing_plugin;
 
+routing_plugin* (*new_plugin_p)(char* host, int port, c_graph_parser *gp, int json_type, int timer_port);
+
+int  (*get_initial_timers_p) (routing_plugin *o, struct timers *t);
+int  (*get_topology_p)       (routing_plugin *o);
+int  (*push_timers_p)        (routing_plugin *o, struct timers t);
+void (*delete_plugin_p)      (routing_plugin *o);
 
 #endif /* SRC_COMMON_H_ */
