f3ab51faf108012c23f5007511d3630b9d93e8ff
Gabriele Gemmi <gabriel@autistici.org>
2018-04-26 12:08:59 +0200

    buggy bug reading netjson from olsrv1

--- prince/src/config.c
+++ prince/src/config.c
@@ -106,10 +106,8 @@ int parse_json_config(const char *filepath, prince_handler_t ph)
 						if (json_object_get_type(val_i)
 						    == json_type_string) {
 							const char *content =
-								json_object_get_string(
-									val_i);
-							ph->proto =
-								strdup(content);
+								json_object_get_string(val_i);
+							ph->proto = strdup(content);
 							completed++;
 						} else {
 							fprintf(stderr,
@@ -117,110 +115,77 @@ int parse_json_config(const char *filepath, prince_handler_t ph)
 							exit(EXIT_FAILURE);
 						}
 
-					} else if (ph->host == 0
-						   && strcmp(key_i, "host")
-							      == 0) {
+					} else if (ph->host == 0 && strcmp(key_i, "host") == 0) {
 						if (json_object_get_type(val_i)
 						    == json_type_string) {
 							const char *content =
-								json_object_get_string(
-									val_i);
-							ph->host =
-								strdup(content);
+								json_object_get_string(val_i);
+							ph->host = strdup(content);
 							completed++;
 						} else {
 							fprintf(stderr,
 								"\"host\" value is not a string\n");
 							exit(EXIT_FAILURE);
 						}
-					} else if (ph->port < 0
-						   && strcmp(key_i, "port")
-							      == 0) {
-						if (json_object_get_type(val_i)
-						    == json_type_int) {
-							ph->port =
-								json_object_get_int(
-									val_i);
+					} else if (ph->port < 0 && strcmp(key_i, "port") == 0) {
+						if (json_object_get_type(val_i) == json_type_int) {
+							ph->port = json_object_get_int(val_i);
 							completed++;
 						} else {
 							fprintf(stderr,
 								"\"port\" value is not an int\n");
 							exit(EXIT_FAILURE);
 						}
-					} else if (ph->port < 0
-						   && strcmp(key_i, "json_type")
-							      == 0) {
+					} else if (strcmp(key_i, "json_type") == 0) {
 						if (json_object_get_type(val_i)
 						    == json_type_string) {
 							const char *content =
-								json_object_get_string(
-									val_i);
-							if (strcmp(content,
-								   "netjson")
-							    == 0) {
-								ph->json_type =
-									1;
-							} else if (
-								strcmp(content,
-								       "jsoninfo")
-								== 0) {
-								ph->json_type =
-									0;
+								json_object_get_string(val_i);
+							if (strcmp(content, "netjson") == 0) {
+								ph->json_type = 1;
+							} else if (strcmp(content, "jsoninfo")
+								   == 0) {
+								ph->json_type = 0;
 							} else {
 								fprintf(stderr,
 									"\"json_type\" value is not specified, will default to netjson");
-								ph->json_type =
-									0;
+								ph->json_type = 0;
 							}
-						} else if (json_object_get_type(
-								   val_i)
+						} else if (json_object_get_type(val_i)
 							   == json_type_int) {
-							ph->json_type =
-								json_object_get_int(
-									val_i);
+							ph->json_type = json_object_get_int(val_i);
 							completed++;
 						} else {
 							fprintf(stderr,
 								"\"json_type\" value is not accepted\n");
 							exit(EXIT_FAILURE);
 						}
-					} else if (strcmp(key_i, "timer_port")
-						   == 0) {
-						if (json_object_get_type(val_i)
-						    == json_type_int) {
-							ph->timer_port =
-								json_object_get_int(
-									val_i);
+					} else if (strcmp(key_i, "timer_port") == 0) {
+						if (json_object_get_type(val_i) == json_type_int) {
+							ph->timer_port = json_object_get_int(val_i);
 							completed++;
 						} else {
 							fprintf(stderr,
 								"\"timer_port\" value is not an int\n");
 							exit(EXIT_FAILURE);
 						}
-					} else if (strcmp(key_i, "refresh")
-						   == 0) {
+					} else if (strcmp(key_i, "refresh") == 0) {
 						if (ph->refresh < 0
-						    && json_object_get_type(
-							       val_i)
+						    && json_object_get_type(val_i)
 							       == json_type_int) {
-							ph->refresh =
-								json_object_get_int(
-									val_i);
+							ph->refresh = json_object_get_int(val_i);
 							completed++;
 						} else {
 							fprintf(stderr,
 								"\"refresh\" value is not an int\n");
 							exit(EXIT_FAILURE);
 						}
-					} else if (strcmp(key_i, "log_file")
-						   == 0) {
+					} else if (strcmp(key_i, "log_file") == 0) {
 						if (json_object_get_type(val_i)
 						    == json_type_string) {
 							const char *content =
-								json_object_get_string(
-									val_i);
-							ph->log_file =
-								strdup(content);
+								json_object_get_string(val_i);
+							ph->log_file = strdup(content);
 						} else {
 							fprintf(stderr,
 								"\"log_file\" value is not a string\n");
@@ -233,96 +198,70 @@ int parse_json_config(const char *filepath, prince_handler_t ph)
 			if (json_object_get_type(val) == json_type_object) {
 				json_object_object_foreach(val, key_i, val_i)
 				{
-					if (!heuristic_set
-					    && strcmp(key_i, "heuristic")
-						       == 0) {
-						if (json_object_get_type(val_i)
-						    == json_type_int) {
+					if (!heuristic_set && strcmp(key_i, "heuristic") == 0) {
+						if (json_object_get_type(val_i) == json_type_int) {
 							ph->heuristic =
-								json_object_get_int(
-									val_i)
-								== 1;
+								json_object_get_int(val_i) == 1;
 							heuristic_set = true;
 						} else {
 							fprintf(stderr,
 								"\"heuristic\" value not an int\n");
 							exit(EXIT_FAILURE);
 						}
-					} else if (strcmp(key_i, "weights")
-						   == 0) {
+					} else if (strcmp(key_i, "weights") == 0) {
 						if (!weights_set
-						    && json_object_get_type(
-							       val_i)
+						    && json_object_get_type(val_i)
 							       == json_type_int) {
 							ph->weights =
-								json_object_get_int(
-									val_i)
-								== 1;
+								json_object_get_int(val_i) == 1;
 							weights_set = true;
 						} else {
 							fprintf(stderr,
 								"\"weights\" value not an int\n");
 							exit(EXIT_FAILURE);
 						}
-					} else if (strcmp(key_i, "recursive")
-						   == 0) {
+					} else if (strcmp(key_i, "recursive") == 0) {
 						if (!recursive_set
-						    && json_object_get_type(
-							       val_i)
+						    && json_object_get_type(val_i)
 							       == json_type_int) {
 							ph->recursive =
-								json_object_get_int(
-									val_i)
-								== 1;
+								json_object_get_int(val_i) == 1;
 							recursive_set = true;
 						} else {
 							fprintf(stderr,
 								"\"recursive\" value not an int\n");
 							exit(EXIT_FAILURE);
 						}
-					} else if (strcmp(key_i,
-							  "stop_unchanged")
-						   == 0) {
+					} else if (strcmp(key_i, "stop_unchanged") == 0) {
 						if (!stop_unchanged_set
-						    && json_object_get_type(
-							       val_i)
+						    && json_object_get_type(val_i)
 							       == json_type_int) {
 							ph->stop_unchanged =
-								json_object_get_int(
-									val_i);
-							stop_unchanged_set =
-								true;
+								json_object_get_int(val_i);
+							stop_unchanged_set = true;
 						} else {
 							fprintf(stderr,
 								"\"stop_unchanged\" value not an int\n");
 							exit(EXIT_FAILURE);
 						}
-					} else if (strcmp(key_i,
-							  "multithreaded")
-						   == 0) {
+					} else if (strcmp(key_i, "multithreaded") == 0) {
 						if (!multithreaded_set
-						    && json_object_get_type(
-							       val_i)
+						    && json_object_get_type(val_i)
 							       == json_type_int) {
 							ph->multithreaded =
-								json_object_get_int(
-									val_i);
-							multithreaded_set =
-								true;
+								json_object_get_int(val_i);
+							multithreaded_set = true;
 						} else {
 							fprintf(stderr,
 								"\"multithreaded\" value not an int\n");
 							exit(EXIT_FAILURE);
 						}
 					} else {
-						fprintf(stderr,
-							"unknowk key \"%s\"\n",
-							key_i);
+						fprintf(stderr, "unknowk key \"%s\"\n", key_i);
 					}
 				}
 			} else {
-				fprintf(stderr,
-					"\"graph-parser\" value not an object\n");
+				fprintf(stderr, "\"graph-parser\" value not an object\n");
 				exit(EXIT_FAILURE);
 			}
 		} else {
@@ -334,8 +273,8 @@ int parse_json_config(const char *filepath, prince_handler_t ph)
 		/* json_object_object_del(jobj, "");*/
 	}
 	free(buffer);
-	if (completed == 5 && heuristic_set && weights_set && recursive_set
-	    && stop_unchanged_set && multithreaded_set) {
+	if (completed == 5 && heuristic_set && weights_set && recursive_set && stop_unchanged_set
+	    && multithreaded_set) {
 		return 0;
 	}
 	return -1;
